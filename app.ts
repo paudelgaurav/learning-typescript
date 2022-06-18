86177b294795a03b69302ccfeee19d292004abe8
Gaurav Paudel <gaurav.paudel@wesionary.team>
2022-06-18 13:27:16 +0545

    feat: addde typed array of string

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 const person: {
     name: string;
     age: number;
+    hobbies: [string, string];
 } = {
     name: 'Ram',
-    age: 10
+    age: 10,
+    hobbies: ['coding', 'sports']
 };
 
 console.log(person.name);
