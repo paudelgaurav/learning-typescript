18be3aaf15719a314ae9b71977c2e6ef43a0752c
Gaurav Paudel <gaurav.paudel@wesionary.team>
2022-06-18 12:59:56 +0545

    chore: project setup

--- typed_script.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var firstInput = document.getElementById("firstInput");
-var secondInput = document.getElementById("secondInput");
-function add(firstInput, secondInput) {
-    return firstInput + secondInput;
-}
-button.addEventListener("click", function () {
-    console.log(add(+firstInput.value, +secondInput.value));
-});
